7f07139edf5913bd3252fb08c6ffc141167f8153
Erik Sundberg <erik.emanuel.sundberg@outlook.com>
2023-05-17 22:01:23 +0200

    flexbox & handle change

--- App.js
+++ App.js
@@ -1,14 +1,40 @@
+import { useState } from 'react';
 import { Button, StyleSheet, Text, TextInput, View } from 'react-native';
 
 export default function App() {
+  const [enteredGoal, setEnteredGoal] = useState('');
+
+  const goalInputHandler = (enteredText) => {
+    console.log(enteredText);
+    setEnteredGoal(enteredText);
+  };
+
+  const [courseGoals, setCourseGoals] = useState([]); //array of strings [
+
+  const addGoalHandler = () => {
+    console.log(enteredGoal);
+    setCourseGoals((currentGoals) => [...currentGoals, enteredGoal]); //spread operator
+    setEnteredGoal('');
+  };
+
   return (
     <View style={styles.appContainer}>
-      <View>
-        <TextInput placeholder="Your course goal" />
-        <Button title="Add Goal" />
+      <View style={styles.inputContainer}>
+        <TextInput
+          value={enteredGoal}
+          onChangeText={goalInputHandler}
+          returnKeyType="done"
+          onSubmitEditing={addGoalHandler}
+          style={styles.textInput}
+          placeholder="Your course goal"
+        />
+        {/* Button does not have a style prop */}
+        <Button title="Add Goal" onPress={addGoalHandler} />
       </View>
-      <View>
-        <Text>List of goals...</Text>
+      <View style={styles.goalsContainer}>
+        {courseGoals.map((goal) => (
+          <Text key={goal}>{goal}</Text>
+        ))}
       </View>
     </View>
   );
@@ -16,11 +42,23 @@ export default function App() {
 
 const styles = StyleSheet.create({
   appContainer: {
-    padding: 50,
-    display: 'flex',
-    flexDirection: 'row',
-    justifyContent: 'space-between',
-    alignItems: 'center',
-    gap: 10,
+    paddingTop: 50,
+    paddingHorizontal: 20,
+    height: '100%', //take up the whole screen height, not just the content height
+    flexDirection: 'column', //main axis is vertical
   },
+  inputContainer: {
+    flex: 1,
+    flexDirection: 'row', //main axis is horizontal
+    justifyContent: 'space-between', //justify along the main axis
+    alignItems: 'center', //align along the cross axis
+  },
+  textInput: {
+    borderWidth: 1,
+    borderColor: 'black',
+    width: '70%',
+    marginRight: 10,
+    padding: 10,
+  },
+  goalsContainer: { flex: 5 },
 });
